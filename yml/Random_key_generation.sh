e850d91638e21257953b6dad3341826187bffe1f
snuglove <snuglovecn@gmail.com>
2020-07-02 10:18:04 +0800

    jumpserver生成秘钥脚本

---
+++ yml/Random_key_generation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#随机生成秘钥
+if [ ! "$SECRET_KEY" ]; then
+  SECRET_KEY=`cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 50`;
+  echo "SECRET_KEY=$SECRET_KEY" >> ~/.env;
+  echo $SECRET_KEY;
+else
+  echo $SECRET_KEY;
+fi  
+if [ ! "$BOOTSTRAP_TOKEN" ]; then
+  BOOTSTRAP_TOKEN=`cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 16`;
+  echo "BOOTSTRAP_TOKEN=$BOOTSTRAP_TOKEN" >> ~/.env;
+  echo $BOOTSTRAP_TOKEN;
+else
+  echo $BOOTSTRAP_TOKEN;
+fi
+
